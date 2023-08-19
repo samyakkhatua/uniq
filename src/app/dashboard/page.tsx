a6f988fddde353645065428bcaf4aec4ba936bb7
samyakkhatua <samyakkhatua@duck.com>
2023-08-20 01:26:56 +0530

    API key

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,21 +5,39 @@ import {
 } from "@/lib/stripe";
 import Link from "next/link";
 
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+const prisma = new PrismaClient();
+
 export default async function Page() {
+  const session = await getServerSession(authOptions);
   const customer = await createCustomerIfNull();
   const hasSubsciption = await hasSubscription();
   const checkoutLink = await createCheckoutLink(String(customer));
 
+  const user = await prisma.user.findFirst({
+    where: {
+      email: session?.user?.email,
+    },
+  });
+
   return (
     <main>
-
       <h5>Dashboard</h5>
 
       <Link href="/dashboard/billing"> Billing </Link>
 
       {hasSubsciption ? (
-        <div className="rounded-md px-4 py-2 bg-emerald-400 font-medium text-white">
+        <div className="flex flex-col gap-4">
+          <div className="rounded-md px-4 py-2 bg-emerald-400 font-medium text-white">
             You have an subscription!
+          </div>
+
+          <div className="divide-y divide-zinc-200 border border-zinc-200 rounded-md">
+            <p className="text-sm text-black px-6 py-4 font-medium">API Key</p>
+            <p className="text-sm font-mono text-zinc-800 px-6 py-8">{user?.api_key}</p>
+          </div>
         </div>
       ) : (
         <>
--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth";
 import Stripe from "stripe";
 
 import { PrismaClient } from "@prisma/client";
+import { randomUUID } from "crypto";
 const prisma = new PrismaClient();
 
 export const stripe = new Stripe(String(process.env.STRIPE_SECRET_KEY), {
@@ -36,9 +37,8 @@ export async function createCheckoutLink(customer: string) {
       {
         price: "price_1NgpiqSDhI13eLY4yFZL8ssn",
       },
-      
     ],
-    mode: "subscription"
+    mode: "subscription",
   });
   return checkout.url;
 }
@@ -50,6 +50,17 @@ export async function createCustomerIfNull() {
     const user = await prisma.user.findFirst({
       where: { email: session.user?.email },
     });
+
+    if (!user?.api_key) {
+      await prisma.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          api_key: "uniq_secret_"+randomUUID(),
+        },
+      });
+    }
 
     if (!user?.stripe_customer_id) {
       const customer = await stripe.customers.create({
